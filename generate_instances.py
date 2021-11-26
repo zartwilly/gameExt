3ea12fbcfb9530508c8fde072e0b8fcebc8260b9
Wilfried EHOUNOU <zartwilly@gmail.com>
2021-11-26 23:40:27 +0100

    gene-instance: generation de n instances

--- generate_instances.py
+++ generate_instances.py
@@ -11,9 +11,154 @@ Each instance has to be at one situation: A, B, C
 import os
 import json
 import numpy as np
-import pandas as pd
+import h5py
+import time
 
 import constances as csts
 
-def test(name_file="data1.json"):
-    f = json.open(os.path.join(csts.data_root, name_file))
+def get_situation(keys_situat, num_pl, m_A, m_B, m_C):
+    """
+    get situation from num_pl. 
+    for example, we have 
+        * m_A players in situation A
+        * m_B players in situation B
+        * m_C players in situation C
+    remember that keys_situat = [Situatiom_A, Situatiom_B, Situatiom_C]
+    """
+    if num_pl < m_A:
+        return keys_situat[0]
+    elif num_pl >= m_A and num_pl < m_A+m_B:
+        return keys_situat[1]
+    elif num_pl >= m_A+m_B and num_pl < m_A+m_B+m_C:
+        return keys_situat[2]
+    else:
+        return csts.SITUATION_ERR
+    
+def get_values_CidizSiSimax(dico_situat):
+    """
+    return values of Ci, z, di, Si, Si_max
+
+    exple: dico_situat = {'Ci': [15], 'z': [5, 16], 'di': 3, 
+                          'Si': [0], 'Si_max': [20]}
+    """
+    Ci = dico_situat["Ci"][0] \
+            if len(dico_situat["Ci"]) == 1 \
+            else np.random.randint(low=dico_situat["Ci"][0], 
+                                    high=dico_situat["Ci"][1]+1)
+    Si = dico_situat["Si"][0] \
+        if len(dico_situat["Si"]) == 1 \
+        else np.random.randint(low=dico_situat["Si"][0], 
+                               high=dico_situat["Si"][1]+1)
+    Si_max = dico_situat["Si_max"][0] \
+        if len(dico_situat["Si_max"]) == 1 \
+        else np.random.randint(low=dico_situat["Si_max"][0], 
+                               high=dico_situat["Si_max"][1]+1)
+    z = np.random.randint(low=dico_situat["z"][0], 
+                          high=dico_situat["z"][1]+1)
+    z_div_2 = int(np.floor(z/2))
+    di = [0, z_div_2, z]
+    
+    return Ci, z, di, Si, Si_max
+
+
+def generate_one_instance(data, situations, n_instance,
+                          m_A=10, m_B=8, m_C=7):
+    cpt_A, cpt_B, cpt_C = 0, 0, 0
+    m_players = m_A + m_B + m_C
+    
+    # generate instances
+    dico_players = dict()
+    for num_pl in range(0, m_players):
+        key_situat = get_situation(keys_situat=situations, 
+                                   num_pl=num_pl, m_A=m_A, m_B=m_B, m_C=m_C)
+        Ci, z, di, Si, Si_max = None, None, None, None, None
+        lock = True
+        if key_situat == situations[0]:
+            Ci, z, di, Si, Si_max = get_values_CidizSiSimax(
+                                        dico_situat=data[key_situat]
+                                        )
+            cpt_A += 1
+            lock = False \
+                if Ci is None or z is None or di is None or Si is None or Si_max is None \
+                else True
+        elif key_situat == situations[1]:
+            prob = np.random.randn()
+            cases_situatB = list(data[key_situat].keys())
+            cases_situatB.sort(reverse=False)
+            key_sitB_05 = cases_situatB[0] if prob<0.5 else cases_situatB[1]
+            Ci, z, di, Si, Si_max = get_values_CidizSiSimax(
+                                        dico_situat=data[key_situat][key_sitB_05]
+                                        )
+            cpt_B += 1
+            lock = False \
+                if Ci is None or z is None or di is None or Si is None or Si_max is None \
+                else True
+        elif key_situat == situations[2]:
+            Ci, z, di, Si, Si_max = get_values_CidizSiSimax(
+                                        dico_situat=data[key_situat]
+                                        )
+            cpt_C += 1
+            lock = False \
+                if Ci is None or z is None or di is None or Si is None or Si_max is None \
+                else True
+        else:
+            print(f"---inst={num_pl}: key={key_situat} Unknown ----")
+            
+        dico_players[csts.PLAYER_ROOT+str(num_pl)] = \
+                {"Ci":Ci, "z":z, "di":di, "Si":Si, "Si_max":Si_max, 
+                 "Situation":key_situat.split("_")[1]} \
+                    if lock \
+                    else None
+            
+    print(f"test_instance_{n_instance}_generated --> OK") \
+        if m_players == cpt_A + cpt_B + cpt_C \
+        else print(f"test_instance_{n_instance}_generated --> NOK: "+
+                   f"m_A={m_A} != cpt_A={cpt_A}, m_B={m_B} != cpt_B={cpt_B},"+
+                   f" m_C={m_C} != cpt_C={cpt_C}")
+        
+    return dico_players
+
+
+def generate_n_instances(file_name="data1_generation.json", n_instances=10,
+                         m_A=10, m_B=8, m_C=7, 
+                         path_2_save=""):
+    # read json file
+    f = open(os.path.join(csts.DATA_ROOT, file_name))
+    data = json.load(f)
+    situations = list(data.keys()); situations.sort(reverse=False)
+    
+    with h5py.File(
+            os.path.join(csts.DATA_ROOT, 
+                         f'data_{n_instances}_instances.hdf5'), 'w') as f:
+        for n_instance in range(n_instances):
+            dico_players_inst = dict()
+            dico_players_inst = generate_one_instance(
+                                    data=data, situations=situations, 
+                                    n_instance=n_instance,
+                                    m_A=m_A, m_B=m_B, m_C=m_C)
+            json_players_inst = json.dumps(dico_players_inst, indent=4)
+            
+            # save to json or h5
+            with open(os.path.join(csts.DATA_ROOT,
+                                   f'data_instance_{n_instance}.json'), 'w') as fp:
+                json.dump(dico_players_inst, fp)
+                
+            # insert to h5py, hdf5
+            dset = f.create_dataset(f'data_instance_{n_instance}', 
+                                    data=json_players_inst
+                                    )
+    
+if __name__ == "__main__":
+    json_file = "data1_generation.json"
+    m_A=10; m_B=8; m_C=7
+    
+    ti = time.time()
+    # n_instances = 10
+    n_instances = 10
+    generate_n_instances(file_name=json_file, 
+                         n_instances=n_instances,
+                         m_A=m_A, m_B=m_B, m_C=m_C)
+    
+    print(f"Runtime = {time.time()-ti}")
+
+    
