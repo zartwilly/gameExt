51da9d0f4ac73d70320d2f0d623603e760fc7c07
Wilfried EHOUNOU <zartwilly@gmail.com>
2021-11-26 16:36:41 +0100

    gene-instance: test comeback commit

--- generate_instances.py
+++ generate_instances.py
@@ -8,4 +8,12 @@ Created on Fri Nov 26 16:09:49 2021
 Generate n instances of game. 
 Each instance has to be at one situation: A, B, C
 """
+import os
+import json
+import numpy as np
+import pandas as pd
 
+import constances as csts
+
+def test(name_file="data1.json"):
+    f = json.open(os.path.join(csts.data_root, name_file))
