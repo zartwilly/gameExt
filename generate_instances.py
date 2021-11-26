a2a7c41a46ff51a83112c67d09c84533e6df6e97
Wilfried EHOUNOU <zartwilly@gmail.com>
2021-11-26 16:12:42 +0100

    gene-instance: premier commit

---
+++ generate_instances.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Nov 26 16:09:49 2021
+
+@author: willy
+
+Generate n instances of game. 
+Each instance has to be at one situation: A, B, C
+"""
+
