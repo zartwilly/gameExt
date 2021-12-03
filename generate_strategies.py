2c039ef694982ab1abb326ca6b8f3659c5ad4cfe
Wilfried EHOUNOU <zartwilly@gmail.com>
2021-12-03 20:15:09 +0100

    gene-strategie: generate players strategies by using new data structures ie dictionary.

--- generate_strategies.py
+++ generate_strategies.py
@@ -52,6 +52,43 @@ def add_fields_players(dico_data):
 #______________________________________________________________________________
 #                       generation of strategies: debut 
 #______________________________________________________________________________
+def compute_strategies_onePlayer_NEW_DSA(dico_values, m_players):
+    """
+    this fonction adds one field "strategies" to dico_values
+    for example:
+    dico_values = {'Ci':15, 'z':11, 'di':[0,5,11], 'Si':0, 'Si_max':20, ... }
+    """
+    possibles_strategies = []
+    for Pi in dico_values["di"]:
+        state_i = None
+        if Pi + dico_values['Si'] <= dico_values['Ci']:
+            state_i = csts.STATES[0]
+            modes = csts.STATE1_STRATS
+            possibles_strategies.extend( list(it.product([Pi], [state_i], modes)) )
+        elif Pi + dico_values['Si'] > dico_values['Ci'] \
+            and Pi <= dico_values['Ci']:
+            state_i = csts.STATES[1]
+            modes = csts.STATE2_STRATS
+            possibles_strategies.extend( list(it.product([Pi], [state_i], modes)) )
+        elif Pi >= dico_values['Ci']:
+            state_i = csts.STATES[2]
+            modes = csts.STATE3_STRATS
+            possibles_strategies.extend( list(it.product([Pi], [state_i], modes)) )
+            
+    # compute probability of each player
+    dico_possibles_strategies_probs = dict()
+    for i, tuple_comb in enumerate(possibles_strategies):
+        dico_values[csts.PROB_NAME+str(i)+"s"] \
+            = [ 1 / len(possibles_strategies) ]
+        list_comb = list(tuple_comb)
+        list_comb.append(csts.PROB_NAME+str(i)+"s")
+        dico_possibles_strategies_probs[csts.PROB_NAME+str(i)+"s"] \
+            = {"Pi": tuple_comb[0], "state_i": tuple_comb[1], 
+               "mode_i": tuple_comb[2]}
+        
+    dico_values["strategies"] = dico_possibles_strategies_probs
+    return dico_values
+
 def compute_strategies_onePlayer(dico_values, m_players):
     """
     this fonction adds one field "strategies" to dico_values
@@ -79,9 +116,9 @@ def compute_strategies_onePlayer(dico_values, m_players):
     possibles_strategies_probs = []
     for i, tuple_comb in enumerate(possibles_strategies):
         dico_values[csts.PROB_NAME+str(i)+"s"] \
-            = [round( len(possibles_strategies) / m_players, 4 )]
+            = [ 1 / len(possibles_strategies) ]
         list_comb = list(tuple_comb)
-        list_comb.append(csts.PROB_NAME+str(i))
+        list_comb.append(csts.PROB_NAME+str(i)+"s")
         possibles_strategies_probs.append( tuple(list_comb) )
         
     dico_values["strategies"] = possibles_strategies_probs
@@ -118,7 +155,76 @@ def generation_strategies_tperiods(dico_data, t_periods):
 #______________________________________________________________________________
 
 
+#______________________________________________________________________________
+#                       generation of strategies NEW DSA: debut 
+#______________________________________________________________________________
+def compute_strategies_onePlayer_NEW_DSA(dico_values, m_players):
+    """
+    this fonction adds one field "strategies" to dico_values
+    for example:
+    dico_values = {'Ci':15, 'z':11, 'di':[0,5,11], 'Si':0, 'Si_max':20, ... }
+    """
+    possibles_strategies = []
+    for Pi in dico_values["di"]:
+        state_i = None
+        if Pi + dico_values['Si'] <= dico_values['Ci']:
+            state_i = csts.STATES[0]
+            modes = csts.STATE1_STRATS
+            possibles_strategies.extend( list(it.product([Pi], [state_i], modes)) )
+        elif Pi + dico_values['Si'] > dico_values['Ci'] \
+            and Pi <= dico_values['Ci']:
+            state_i = csts.STATES[1]
+            modes = csts.STATE2_STRATS
+            possibles_strategies.extend( list(it.product([Pi], [state_i], modes)) )
+        elif Pi >= dico_values['Ci']:
+            state_i = csts.STATES[2]
+            modes = csts.STATE3_STRATS
+            possibles_strategies.extend( list(it.product([Pi], [state_i], modes)) )
+            
+    # compute probability of each player
+    dico_possibles_strategies_probs = dict()
+    for i, tuple_comb in enumerate(possibles_strategies):
+        dico_values[csts.PROB_NAME+str(i)+"s"] \
+            = [ 1 / len(possibles_strategies) ]
+        list_comb = list(tuple_comb)
+        list_comb.append(csts.PROB_NAME+str(i)+"s")
+        dico_possibles_strategies_probs[csts.PROB_NAME+str(i)+"s"] \
+            = {"Pi": tuple_comb[0], "state_i": tuple_comb[1], 
+               "mode_i": tuple_comb[2]}
+        
+    dico_values["strategies"] = dico_possibles_strategies_probs
+    return dico_values
+            
+def generation_strategies_NEW_DSA(dico_data, t_period=2):
+    """
+    dico_data : {"t_0": {'player_0':{}, 'player_1':{}}, 
+                 "t_1": {'player_0':{}, 'player_1':{}} }
 
+    """
+    dico_data_t = dico_data[csts.PERIOD_ROOT+str(t_period)]
+    m_players = len( list(dico_data_t.keys()) )
+    for player_name, dico_values in dico_data_t.items():
+        dico_values = compute_strategies_onePlayer_NEW_DSA(
+                        dico_values=dico_values, 
+                        m_players=m_players)
+        
+    return  dico_data_t
+        
+def generation_strategies_tperiods_NEW_DSA(dico_data, t_periods):
+    """
+    dico_data : {"t_0": {'player_0':{}, 'player_1':{}}, 
+                 "t_1": {'player_0':{}, 'player_1':{}} }
+
+    """
+    for t_period in range(0, t_periods):
+        dico_data[csts.PERIOD_ROOT+str(t_period)] \
+            = generation_strategies_NEW_DSA(dico_data, t_period=t_period)
+        
+    return dico_data
+
+#______________________________________________________________________________
+#                       generation of strategies: fin 
+#______________________________________________________________________________
 
 
 if __name__ == "__main__":
@@ -144,4 +250,12 @@ if __name__ == "__main__":
         = generation_strategies_tperiods(
             dico_data=dico_players_insti_tperiods, 
             t_periods=t_periods)
+        
+    # test NEW_DSA
+    dico_players_insti_tperiods_NEW_DSA \
+        = generation_strategies_tperiods_NEW_DSA(
+            dico_data=dico_players_insti_tperiods, 
+            t_periods=t_periods)
+    
+    
     
