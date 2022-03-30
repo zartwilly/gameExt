b5ea820f5f535b7489b77f5da9b0e15222fc87c0
Wilfried EHOUNOU <zartwilly@gmail.com>
2022-03-30 15:23:06 +0200

    gameExt2-DBG: fonction chosen strategies for all players - Coder et tester- OK.

--- gameExt2_lri.py
+++ gameExt2_lri.py
@@ -26,10 +26,10 @@ from pathlib import Path
 #______________________________________________________________________________
 #                       choose strategy for all players: debut
 #______________________________________________________________________________
-def choose_f21_strategy_4_all_players(dico_tperiods_players, t_period):
+def choose_f12_strategy_4_all_players(dico_insti_tperiods_players, t_period, r_step):
     """
-    choose f21_strategy for all players by following distribution 
-    p_0s, p_1s, p_2s
+    choose f12_strategy for all players by following distribution 
+    r_0_s, r_1_s, r_2s
     
     NB:
         dico_tperiods_players = {"t_j":{'player_i':dico_t_player_i, 
@@ -39,40 +39,61 @@ def choose_f21_strategy_4_all_players(dico_tperiods_players, t_period):
                              }
         avec 
         t_period = 't_j' with j = {0,1,..., N}
-        player_ = 'player_i' with i = {0,1,..., M} and 
+        player_ = 'player_i' with i = {0,1,..., M} and
+        r_step \in {0, ..., 100} 
         dico_t_player_i = {keys=csts.LEARNING_PROPERTIES, 
-                           values=[values' list of LEARNING_PROPERTIES]}
+                           values=[values' list of LEARNING_PROPERTIES],
+                            r_0_s = [r_0_1, r_0_2, ..., r_0_100]
+                         r_1_s = [r_1_1, r_1_2, ..., r_1_100]
+                         r_2_s = [r_2_1, r_2_2, ..., r_2_100]
+                         f12_strategies_names = ["r_0_s", "r_1_s", "r_2_s"]
+                         f12_strategies_names_is = ["r_0_s"^1, "r_0_s"^2, ...,"r_2_s"^100]   
+                            }
     """
-    dico_chosen_strats_k = dict()     # key : player_X, values = dict()
-    for player_i in dico_tperiods_players[t_period].keys():
-        is_playing = dico_tperiods_players[t_period][player_i]["is_playing"][-1]
+    dico_chosen_strats_r = dict()     # key : player_X, values = dict()
+    for player_i in dico_insti_tperiods_players[t_period].keys():
+        is_playing = dico_insti_tperiods_players[t_period][player_i]["is_playing"][-1]
         if is_playing:
             keys_probas \
                 = list(
-                    filter(lambda x: x.startswith(csts.PROB_NAME), 
-                           list(dico_tperiods_players[t_period][player_i].keys()) 
+                    filter(lambda x: x.startswith(csts.F21_NAME) \
+                                       and x.endswith("_s"), 
+                           list(dico_insti_tperiods_players[t_period][player_i].keys()) 
                           )
                     )
             vals_probas \
                 = list( 
-                    map(lambda x: dico_tperiods_players\
-                                    [t_period][player_i][x][-1],
+                    map(lambda x: dico_insti_tperiods_players\
+                                    [t_period][player_i][x]["prob"],
                         keys_probas) 
                     )
             #print("proba = {}".format( list(zip(keys_probas,vals_probas)) ))
             
-            f21_strategy_name_i = np.random.choice(keys_probas, p=vals_probas)
-            dico_PiStateiS1S2 = dico_tperiods_players\
-                                    [t_period][player_i]\
-                                    ["f21_strategies"][f21_strategy_name_i]
-                
-            dico_chosen_strats_k[player_i] \
-                = {"f21_strategy_name_i": f21_strategy_name_i, 
-                   "state_i": dico_PiStateiS1S2['state_i'],
-                   "Pi": dico_PiStateiS1S2['Pi']
-                   }
+            f12_strategy_name_i = np.random.choice(keys_probas, p=vals_probas)
+            dico_insti_tperiods_players\
+                [t_period][player_i]\
+                    ["f12_strategies_names_is"].append(f12_strategy_name_i)
+                    
+            # save variables values of player_i to dictionnary
+            dico_prob_Pi_p_01s = dict()
+            dico_prob_Pi_p_01s["Pi"] = dico_insti_tperiods_players\
+                                        [t_period][player_i]\
+                                        [f12_strategy_name_i]["di"]
+            dico_prob_Pi_p_01s["p_0s"] = dico_insti_tperiods_players\
+                                            [t_period][player_i]\
+                                            [f12_strategy_name_i]["p_0s"][-1]
+            dico_prob_Pi_p_01s["p_1s"] = dico_insti_tperiods_players\
+                                            [t_period][player_i]\
+                                            [f12_strategy_name_i]["p_1s"][-1]
+            dico_prob_Pi_p_01s["prob"] = dico_insti_tperiods_players\
+                                            [t_period][player_i]\
+                                            [f12_strategy_name_i]["prob"]
+            dico_prob_Pi_p_01s["f12_strategy_name_i"] = eval(f12_strategy_name_i)
             
-    return dico_chosen_strats_k 
+                
+            dico_chosen_strats_r[player_i] = dico_prob_Pi_p_01s
+            
+    return dico_chosen_strats_r
 #______________________________________________________________________________
 #                       choose strategy for all players: fin
 #______________________________________________________________________________  
@@ -118,9 +139,22 @@ def game_ext2(dico_insti_tperiods_players, args):
     for t_period in dico_insti_tperiods_players.keys():
         print(f'**** period : {t_period} ****')
         
+        r_step = 0
+        while r_step < args['r_steps']:
+            dico_chosen_strats_r \
+                = choose_f12_strategy_4_all_players(
+                    dico_insti_tperiods_players=dico_insti_tperiods_players, 
+                    t_period=t_period, 
+                    r_step=r_step)
+                
+            # incrementation of r_step
+            r_step += 1
+            
+            
+                                   
         for r in range(0, args['r_steps']):
             dico_chosen_strats_k \
-                = choose_f21_strategy_4_all_players(
+                = choose_f12_strategy_4_all_players(
                     dico_insti_tperiods_players=dico_insti_tperiods_players, 
                     t_period=t_period)
                 
@@ -145,8 +179,10 @@ def game_ext2(dico_insti_tperiods_players, args):
 #______________________________________________________________________________
 #                       test : debut
 #______________________________________________________________________________
-def test_gameExt2(args):
-    
+def read_json_dataset_initialise_strategies(args):
+    """
+    read and initialise strategies to all players
+    """
     dico_insti_tperiods_players \
         = gene_strats.read_json_filename(json_filename=args['json_filename'])
         
@@ -157,7 +193,27 @@ def test_gameExt2(args):
         = gene_strats.generate_strategies_f21_f22_players(
             dico_insti_tperiods_players=dico_insti_tperiods_players, 
             r_steps=args["r_steps"])
+        
+    return dico_insti_tperiods_players
+
+def test_chosen_strats_r(args):
+    dico_insti_tperiods_players = read_json_dataset_initialise_strategies(args)
+    t_period = np.random.choice(range(args["t_periods"]))
+    t_period = csts.PERIOD_ROOT + str(t_period)
     
+    dico_chosen_strats_k \
+        = choose_f12_strategy_4_all_players(
+            dico_insti_tperiods_players=dico_insti_tperiods_players, 
+            t_period=t_period,
+            r_step=args["r_steps"])
+        
+    return dico_chosen_strats_k
+
+def test_gameExt2(args):
+    
+    dico_insti_tperiods_players = read_json_dataset_initialise_strategies(args) 
+    
+
     return dico_insti_tperiods_players
     # # test generations strategies
     # dico_insti_tperiods_players \
@@ -202,5 +258,7 @@ if __name__ == "__main__":
             }
     
     dico_insti_tperiods_players = test_gameExt2(args)
+    # test chosen strategies
+    dico_chosen_strats_k = test_chosen_strats_r(args)
     
     print(f"runtime = {time.time() - ti}")
